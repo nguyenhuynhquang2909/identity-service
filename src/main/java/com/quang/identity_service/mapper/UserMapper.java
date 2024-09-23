be4c027da489c68a327ef1315292312fe11b1c41
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-09-23 15:26:50 +0700

    Refactor code with MapStruct

--- src/main/java/com/quang/identity_service/mapper/UserMapper.java
+++ src/main/java/com/quang/identity_service/mapper/UserMapper.java
@@ -1,5 +1,16 @@
 package com.quang.identity_service.mapper;
 
-public interface UserMapper {
+import com.quang.identity_service.dto.request.UserCreationRequest;
+import com.quang.identity_service.dto.request.UserUpdateRequest;
+import com.quang.identity_service.dto.response.UserResponse;
+import com.quang.identity_service.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toUser(UserCreationRequest request);
+    UserResponse toUserResponse(User user);
+    void updateUser(@MappingTarget  User user, UserUpdateRequest request);
 }
--- src/main/java/com/quang/identity_service/service/UserService.java
+++ src/main/java/com/quang/identity_service/service/UserService.java
@@ -2,31 +2,32 @@ package com.quang.identity_service.service;
 
 import com.quang.identity_service.dto.request.UserCreationRequest;
 import com.quang.identity_service.dto.request.UserUpdateRequest;
+import com.quang.identity_service.dto.response.UserResponse;
 import com.quang.identity_service.entity.User;
 import com.quang.identity_service.exception.AppException;
 import com.quang.identity_service.exception.ErrorCode;
+import com.quang.identity_service.mapper.UserMapper;
 import com.quang.identity_service.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+    UserRepository userRepository;
+    UserMapper userMapper;
 
     public User createUser(UserCreationRequest request) {
-        User user = new User();
         if (userRepository.existsByUsername(request.getUsername())) {
             throw new AppException(ErrorCode.USER_EXISTED);
         }
-
-        user.setUsername(request.getUsername());
-        user.setPassword(request.getPassword());
-        user.setFirstName(request.getFirstName());
-        user.setLastName(request.getLastName());
-        user.setDob(request.getDob());
+        User user = userMapper.toUser(request);
 
         return  userRepository.save(user);
     }
@@ -35,20 +36,17 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public User getUser(String id) {
-        return userRepository
-                .findById(id)
-                .orElseThrow(() ->  new RuntimeException("User not found"));
+    public UserResponse getUser(String id) {
+        return userMapper.toUserResponse(userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found")));
 
     }
 
-    public User updateUser(String userId,UserUpdateRequest request) {
-        User user = getUser(userId);
-        user.setPassword(request.getPassword());
-        user.setFirstName(request.getFirstName());
-        user.setLastName(request.getLastName());
-        user.setDob(request.getDob());
-        return userRepository.save(user);
+    public UserResponse updateUser(String userId,UserUpdateRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new RuntimeException("User not found"));
+        userMapper.updateUser(user, request);
+        return userMapper.toUserResponse(userRepository.save(user));
     }
 
     public void deleteUser(String userId) {
