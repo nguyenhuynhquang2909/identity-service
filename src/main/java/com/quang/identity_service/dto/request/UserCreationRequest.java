f008f43987fa3defce200eff532f643e935bd3a0
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-09-23 12:51:02 +0700

    Add validation and exception handling

--- src/main/java/com/quang/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/quang/identity_service/dto/request/UserCreationRequest.java
@@ -1,12 +1,20 @@
 package com.quang.identity_service.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class UserCreationRequest {
+    @Size(min = 3, message = "Username must be at least 3 characters  ")
     private String username;
+
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
     private String firstName;
+
     private String lastName;
+
     private LocalDate dob;
 
     public String getUsername() {
